fba5e4720457d2024755c7e1d58e1ba9465d8278
raflyrf <27112000+raflyrf@users.noreply.github.com>
2021-11-10 12:59:41 +0700

    tabel admin, proposal, banner

---
+++ database/migrations/2021_11_10_034012_create_banners_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('adminId')->unsigned();
+            $table->string('nama');
+            $table->date('uploadTime');
+            $table->string('link');
+
+            $table->foreign('adminId')->references('id')->on('admins')
+            ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
---
+++ database/migrations/2021_11_10_034541_create_proposal_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProposalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('adminId')->unsigned();
+            $table->string('kabinet');
+            $table->date('uploadTime');
+            $table->string('eventName');
+            $table->string('eventTimeHeld');
+
+            $table->foreign('adminId')->references('id')->on('admins')
+            ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal');
+    }
+}
