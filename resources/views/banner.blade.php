7ef8d31ea48499228c45fb2be3c756edb7511726
syukriyatulhanifa <syukriyatul_hanifa@apps.ipb.ac.id>
2021-12-02 09:56:42 +0700

    t

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid ps-md-0" style="padding:110px 0 50px 0; background:#2277BF">
-    <h1 class="fakultas-title">BEM KM IPB</h1>
     @foreach($bannernya as $key => $p)
+    <h1 class="fakultas-title">BEM {{$p->nama}}</h1>
     <marquee behavior="scroll" direction="right" class="banner-deskripsi">{{$p->link}}</marquee>
     @endforeach
 </div>
